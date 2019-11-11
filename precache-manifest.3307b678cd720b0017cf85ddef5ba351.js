905d9f5aed6fb8887a0249bc44843d4fc2eccd82
Badavrov <badavrow@gmail.com>
2019-11-11 10:04:39 +0200

    Updates

---
+++ precache-manifest.3307b678cd720b0017cf85ddef5ba351.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b68c031053e149d8254e7d5a5a5d887",
+    "url": "/lottie-test/index.html"
+  },
+  {
+    "revision": "51b121f12e7b273efef0",
+    "url": "/lottie-test/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "2347a1cbfa6d915d11c8",
+    "url": "/lottie-test/static/js/2.7ffd0399.chunk.js"
+  },
+  {
+    "revision": "51b121f12e7b273efef0",
+    "url": "/lottie-test/static/js/main.552c552d.chunk.js"
+  },
+  {
+    "revision": "027f69a31e7f42280bd2",
+    "url": "/lottie-test/static/js/runtime-main.40934255.js"
+  }
+]);
